05aaf55d2b8501409bc48554332cf27d4d373c7c
Artem <Artem@localhost.localdomain>
2016-09-09 16:32:38 +0300

    Console menu

--- HungryOwl/HungryOwl/src/HungryOwl.cpp
+++ HungryOwl/HungryOwl/src/HungryOwl.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "---<<<   HUNGRY OWL   >>>---" << std::endl;
+	std::cout << "1. New game" << std::endl;
+	std::cout << "2. Exit" << std::endl;
+	std::cout << "Select:   " << std::endl;
+	int menuChoice = 0;
+	while(menuChoice != 1 || menuChoice != 2)
+	{
+		std::cin >> menuChoice;
+		switch(menuChoice)
+		{
+		case 1:
+			{
+				break;
+			}
+		case 2:
+			{
+				break;
+			}
+		default:
+			{
+				std::cout << "Incorrect choice, try again:" << std::endl;
+			}
+		}
+	}
 	return 0;
 }
